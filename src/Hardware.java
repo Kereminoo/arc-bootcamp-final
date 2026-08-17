28666d89ed595b9bf9f3e27b61327990fddd2a02
Kerem <155567936+Kereminoo@users.noreply.github.com>
2026-08-17 21:15:15 +0300

    Add getters, illegal argument exceptions to constructor and a sell function in Hardware

--- src/Hardware.java
+++ src/Hardware.java
@@ -5,6 +5,18 @@ public abstract class Hardware {
     private double unitCost;
 
     public Hardware(int partID, String displayName, int amountInStock, double unitCost) {
+        if (partID <= 0) {
+            throw new IllegalArgumentException("PartID must be at least 1.");
+        }
+        if (displayName == null || displayName.equals("")) {
+            throw new IllegalArgumentException("Hardware must have a display name.");
+        }
+        if (amountInStock < 0) {
+            throw new IllegalArgumentException("Hardware cannot have negative stock.");
+        }
+        if (unitCost <= 0) {
+            throw new IllegalArgumentException("Hardware cannot be free or cost negative money (we ain't giving money away bro).");
+        }
         this.partID = partID;
         this.displayName = displayName;
         this.amountInStock = amountInStock;
@@ -25,5 +37,12 @@ public abstract class Hardware {
 
     public double getUnitCost() {
         return unitCost;
+    }
+
+    public void sellHardware(int amountSold) {
+        if (amountInStock - amountSold < 0) {
+            throw new IllegalArgumentException("There's not enough stock to sell " + amountSold + getDisplayName() + "(s).");
+        }
+        amountInStock -= amountSold;
     }
 }
