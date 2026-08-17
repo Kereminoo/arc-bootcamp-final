b2166da99aad5fa015f5cc17a614c837d1414ca5
Kerem <155567936+Kereminoo@users.noreply.github.com>
2026-08-17 23:10:35 +0300

    Implement Bolt

---
+++ src/Bolt.java
@@ -0,0 +1,39 @@
+public class Bolt extends Hardware{
+    private double lengthMm;
+    private double diameterMm;
+    private HeadType headType;
+
+    public Bolt(int partID, String displayName, int amountInStock, double unitCost, double lengthMm, double diameterMm, HeadType headType) {
+        super(partID, displayName, amountInStock, unitCost);
+
+        if (lengthMm <= 0) {
+            throw new IllegalArgumentException("Length (mm) cannot be zero or below!");
+        }
+        if (diameterMm <= 0) {
+            throw new IllegalArgumentException("Diameter (mm) cannot be zero or below!");
+        }
+
+        this.lengthMm = lengthMm;
+        this.diameterMm = diameterMm;
+        this.headType = headType;
+    }
+
+    @Override
+    public String getCategoryDetails() {
+        return String.format("Category: Bolt, PartID: %d, Display Name: %s, Amount Left In Stock: %d, Unit Cost: %f, Length (mm): %f, Diameter (mm): %f, Head Type: %s.",
+                                                  getPartID(), getDisplayName(), getAmountInStock(), getUnitCost(), lengthMm, diameterMm, headType 
+        );
+    }
+
+    public double getLengthMm() {
+        return lengthMm;
+    }
+
+    public double getDiameterMm() {
+        return diameterMm;
+    }
+
+    public HeadType getHeadType() {
+        return headType;
+    }
+}
---
+++ src/HeadType.java
@@ -0,0 +1,3 @@
+public enum HeadType {
+    HEX, SOCKET, PHILLIPS, FLAT
+}
