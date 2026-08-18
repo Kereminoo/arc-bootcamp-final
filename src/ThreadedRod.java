9abd4aecec3d69082bd4dba70e56d77b0f6acfdf
Kerem <155567936+Kereminoo@users.noreply.github.com>
2026-08-18 20:11:55 +0300

    Implement ThreadedRod

---
+++ src/ThreadedRod.java
@@ -0,0 +1,40 @@
+public class ThreadedRod extends Hardware {
+    public double diameterMm;
+    public double lengthMeters;
+    public double threadPitchMm;
+
+    public ThreadedRod(int partID, String displayName, int amountInStock, double unitCost, double diameterMm, double lengthMeters, double threadPitchMm) {
+        super(partID, displayName, amountInStock, unitCost);
+        if (diameterMm <= 0) {
+            throw new IllegalArgumentException("Diameter cannot be zero or negative!");
+        }
+        if (lengthMeters <= 0) {
+            throw new IllegalArgumentException("Length cannot be zero or negative!");
+        }
+        if (threadPitchMm <= 0) {
+            throw new IllegalArgumentException("Thread pitch cannot be zero or negative!");
+        }
+        this.diameterMm = diameterMm;
+        this.lengthMeters = lengthMeters;
+        this.threadPitchMm = threadPitchMm;
+    }
+
+    @Override
+    public String getCategoryDetails() {
+        return String.format("Category: Bolt, PartID: %d, Display Name: %s, Amount Left In Stock: %d, Unit Cost: %f, Diameter (mm): %f, Length (m): %f, Thread Pitch (mm): %f.",
+                                                  getPartID(), getDisplayName(), getAmountInStock(), getUnitCost(), diameterMm, lengthMeters, threadPitchMm
+        );
+    }
+    
+    public double getDiameterMm() {
+        return diameterMm;
+    }
+
+    public double getLengthMeters() {
+        return lengthMeters;
+    }
+
+    public double getThreadPitchMm() {
+        return threadPitchMm;
+    }
+}
